a0e41c7e56d331e07bb4260cda4c8826df67e751
yashrawat <yashrawat682@gmail.com>
2025-04-16 14:50:39 +0530

    initialized all the routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,41 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+
+
+
+app.post('/api/v1/user/signup',(c)=>{
+  
+  return c.text("hello")}
+
+)
+
+app.post('/api/v1/user/signin',(c)=>{
+  
+  return c.text("hello")}
+
+)
+
+app.post('/api/v1/blog',(c)=>{
+  
+  return c.text("hello")}
+
+)
+
+app.get('/api/v1/blog/bulk', (c) => {
+
+  return c.text('Hello Hono!')
+}
+
+)
+
+app.put('/api/v1/blog',(c)=>{
+  
+  return c.text('hello hono')}
+
+)
+
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
